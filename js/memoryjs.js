a47407a7f63f548d494265c04685d26ccdb71778
TW-Pride <b2khakashu@yahoo.com>
2019-09-10 20:58:53 -0700

    created game logic

--- js/memoryjs.js
+++ js/memoryjs.js
@@ -1,21 +1,39 @@
-var cards = [{
-        rank: "queen",
+/*
+var cardOne = "QUEEN";
+var cardTwo = cardOne;
+var cardThree = "KING";
+var cardFour = cardThree;
+
+
+console.log("user Flipped " + cardOne);
+console.log("user Flipped " + cardTwo);
+console.log("user Flipped " + cardThree);
+console.log("user Flipped " + cardFour);
+*/
+
+var cards = [
+    {
+        rank: "QUEEN",
         suit: "hearts",
         cardImage: "images/queen-of-hearts.png"
-    },{
-        rank: "queen",
+    },
+    {
+        rank: "QUEEN",
         suit: "diamonds",
         cardImage: "images/queen-of-diamonds.png"
-    },{
-        rank: "king",
+    },
+    {
+        rank: "KING",
         suit: "hearts",
         cardImage: "images/king-of-hearts.png"
-    },{
-        rank: "king",
+    },
+    {
+        rank: "KING",
         suit: "diamonds",
         cardImage: "images/king-of-diamonds.png"
-    }];
-    
+    }
+];
+
 var cardsInPlay = [];
 
 
@@ -31,22 +49,33 @@ function checkForMatch() {
 
 function flipCard(cardId) {
     var cardId = this.getAttribute('data-id');
-    console.log("User flipped " + cards[cardId]);
+    console.log("User flipped " + cards[cardId].rank);
     cardsInPlay.push(cards[cardId].rank);
-    console.log(cards[cardId].cardImage);
     console.log(cards[cardId].suit);
-
+    console.log(cards[cardId].cardImage);
     
     this.setAttribute("src", cards[cardId].cardImage);
     if (cardsInPlay.length === 2) {
     if (cardsInPlay[0] === cardsInPlay[1]) {
-        alert("Congrats! You have found the Pairs");
-    } else {
-        alert("Epic Fail, try again!");
+        alert("Congrats! You have found the Pair's");
+        } else {
+            alert("Epic Fail, try again!");
+        }
     }
 }
 
+/*
+var flipCard = function(cardId){
+
+    if(cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]){
+
+        alert("You found a match!");
+    }else{ 
+        alert("Sorry, try again!");
+    };
+    console.log("User filpped " + cards[cardId]);
 }
+*/
 
 function createBoard() {
     for (i = 0; i < cards.length; i++) {
